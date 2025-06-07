a440cf5ecbca660a26a6c11730b9bb2e4c4da574
Tango-bit <varunrb1994@gmail.com>
2025-06-07 11:24:20 +0200

    Add project 3

---
+++ Project 3/src/chatbot.h
@@ -0,0 +1,60 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+    bool _is_destructed = false;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+    
+    // copy constructor
+    ChatBot(const ChatBot &chatbot);
+
+    // copy assignment operator
+    ChatBot &operator=(const ChatBot &chatbot);
+
+    // move constructor
+    ChatBot(ChatBot &&chatbot);
+
+    // move assignment operator
+    ChatBot &operator=(ChatBot &&chatbot);
+
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    ChatLogic* GetChatLogicHandle() { return _chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
